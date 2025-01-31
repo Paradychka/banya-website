d43c75930c1a3ab9613c0647edfca8e9790e91b4
Paradychka <verbicpetr@gmail.com>
2025-01-31 17:41:31 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Дякуємо за ваше повідомлення! Ми зв\'яжемося з вами найближчим часом.');
+    // Ви можете додати функціонал для відправки форми на сервер тут
+});
